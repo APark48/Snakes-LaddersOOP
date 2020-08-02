cba7d72580a1de4e8bb7af739157ff3376852774
APark48 <alfredpark887@hotmail.com>
2020-08-02 15:43:30 -0500

    Added player selection screen
    Using a do while loop and a switch statement, added a selection screen for the player to exit the game or end the game

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
 #include "Dice.h"
 #include "Game.h"
 
+void display(){
+    std::cout << "SNAKES & LADDERS GAME \nENTER '1' TO PLAY OR '0' TO QUIT" << std::endl;    
+}
+int selection(){
+    int select;
+    std::cin >> select;
+    return select;
+}
+
 int main(){
-    Game g;
-    g.startGame();
+    display();
+    char select = selection();
+    do {
+        switch (select)
+        {
+        case 1:
+            std::cout << "Game started successfully." << std::endl;
+            break;
+        case 0:
+            std::cout << "Game exited successfully." << std::endl;
+            break;
+        default:
+            std::cout << "Invalid input." << std::endl;
+            break;
+        }
+    } while (select != 0);
+    
+    //Game g;
+    //g.startGame();
     return 0;
 }
